4ccf82b5cf330c0055df22de0bf15607c378f057
Tasnimwheebi <tasnimwheebi@gmail.com>
2021-05-16 20:33:16 +0300

    lab-01

---
+++ _tests_/server.test.js
@@ -0,0 +1,23 @@
+'use strict';
+const server = require('../server.js');
+const supertest = require('supertest');
+// const { request } = require('express');
+const request = supertest(server.server);
+
+describe('server',()=>{
+  it('should get 404 status',async()=>{
+    const response = await request.get('/go');
+    expect(response.status).toBe(404);
+  });
+  it('should get a welcome message',async()=>{
+    let route = '/';
+    const response = await request.get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Welcome to my web App');
+  });
+  it('should get an error', async()=>{
+const response = await request.get('/bad');
+expect(response.status).toEqual(500);
+  });
+});
+
---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+const server = require('./server');
+require('dotenv').config();
+const PORT = process.env.PORT || 4000;
+server.start(PORT);
